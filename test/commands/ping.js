ddddad7654ae75d5915ea22dfbea4fd890bd08e0
joakin <joaquin@chimeces.com>
2016-05-05 10:23:19 -0600

    Add negative test case to ping

--- test/commands/ping.js
+++ test/commands/ping.js
@@ -1,7 +1,8 @@
 const test = require('tape')
 const ping = require('../../lib/commands/ping')()
 
-test('responds to ping', (t) => {
-  t.plan(1)
+test('only responds to ping', (t) => {
+  t.plan(2)
   t.deepEqual(ping('someone', 'ping'), ['pong'], 'returns a pong message when receiving ping')
+  t.notOk(ping('someone', 'pingoo'), 'returns a pong message when receiving ping')
 })
