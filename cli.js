5ff71eab92d0c7a791adae890a75683218c1e7a7
joakin <joaquin@chimeces.com>
2016-05-05 15:30:19 -0600

    AAhg demo

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ const readline = require('readline')
 const send = (from, msg) => console.log(msg)
 
 const commands = require('./lib/commands')
-const message = require('./lib/message')(commands, send)
+const message = require('./lib/message')('cli', commands, send)
 
 const rl = readline.createInterface({
   input: process.stdin,
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const email = 'vickypediabot@gmail.com'
-const password = process.env.HANGOUTSPASSWORD
+const hangouts = require('./lib/hangouts')
+const config = require(process.env.CONFIG || __dirname + '/conf.json')
+const irc = require('./lib/irc')
 
 require('./lib/server')
 
 if (process.env.NODE_ENV === 'production') {
-  const HangoutsBot = require('hangouts-bot')
-
-  const bot = new HangoutsBot(email, password)
-  const send = bot.sendMessage.bind(bot)
-
-  const commands = require('./lib/commands')
-  const message = require('./lib/message')(commands, send)
-
-  bot.on('online', () => { console.log('online') })
-
-  bot.on('message', (from, msg) => message(from, msg))
+  hangouts(config.hangouts.user, config.hangouts.password)
+  irc(config.irc.server, config.irc.channels, config.irc.user, config.irc.password)
 }
